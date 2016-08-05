6fc9f96e25932a440da05fb326363652b8202ff6
serg <aspirin_1988@mail.ru>
2016-08-05 10:29:56 +0600

    /start

---
+++ wp-content/themes/classico/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Classico Theme Customizer.
+ *
+ * @package Classico
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function classico_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'classico_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function classico_customize_preview_js() {
+	wp_enqueue_script( 'classico_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'classico_customize_preview_js' );
