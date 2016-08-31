a843839bafcc3081450f4ea50c26237a94cbba3f
Sergey Demidov <aspirin_1988@mail.ru>
2016-08-31 14:40:27 +0600

    /start

--- wp-content/themes/classico/archive.php
+++ wp-content/themes/classico/archive.php
@@ -7,6 +7,12 @@
  * @package Classico
  */
 $obj=get_queried_object();
+$query_url=$_SERVER['REDIRECT_URL'];
+$post=count(get_posts(array('category_name'=>$obj->slug,'posts_per_page'=>-1)));
+$page_post=get_option('posts_per_page');
+$page_count=ceil($post/$page_post);
+$page_num=(int)get_query_var('page');
+$offset=$page_num*$page_post;;
 get_header(); ?>
 
 	<div id="primary" class="content-area">
@@ -59,10 +65,25 @@ get_header(); ?>
 							while ( have_posts() ) : the_post();
 								get_template_part( 'template-parts/content', 'archive-single' );
 							endwhile;
-							the_posts_navigation();
 						endif; ?>
 					</ul>
 				</div>
+			</div>
+			<div class="services">
+			<?php if($page_count>1):?>
+				<!--НАЧАЛО пагинация-->
+				<ul class="uk-pagination">
+					<li style="display: inline-block" ><a href="<?=$query_url?>?page=0"><i class="uk-icon-angle-double-left"></i></a></li>
+					<?php for ($i=0; $i<$page_count; $i++): $class=''; if ($i==$page_num){$class='class="uk-active"';} ?>
+						<li style="display: inline-block" data-id="<?=$i?>" data-id1="<?=$page_num?>" <?=$class?> >
+							<a href="<?=$query_url.'?page='.$i?>">
+								<?=$i+1?>
+							</a>
+						</li>
+					<?php endfor; ?>
+					<li style="display: inline-block" ><a href="<?=$query_url?>?page=<?=$page_count-1?>"><i class="uk-icon-angle-double-right"></i></a></li>
+				</ul>
+			<?php endif; ?>
 			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
